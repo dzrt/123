f05e84b45f1cd72806c0383de59f0d942440c556
bosingwaa <felipemr000@gmail.com>
2016-03-11 23:34:30 +0000

    added Prototipo

---
+++ Prototipo/Prototipo/src/backend/main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+import frontend.LOG;
+
+/**
+ *
+ * @author Felipe
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LOG l = new LOG();
+        l.setVisible(true);
+
+        /*ligarbd l = new ligarbd();
+        System.out.print(l.existe("root","root"));
+*/
+}
+    
+}
